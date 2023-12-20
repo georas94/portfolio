25a6593b6483b8d441c1dbb7d10f02cabe61e62a
georas94 <rashid.tamboura@icloud.com>
2023-12-20 13:15:43 +0100

    Fere : Update navbar behaviour for mobile screens

--- fere/app/public/js/base.js
+++ fere/app/public/js/base.js
@@ -1,16 +1,27 @@
 $(document).ready(function () {
     $('.menu li').click(function (event) {
         event.preventDefault();
-        $('.menu div').each(function () {
-                $(this).addClass('hidden');
-        })
         let selector = $('.sub-'+$(this).attr('class'));
-        if (selector.hasClass('show')){
+        if (selector.hasClass('show') && !(selector.hasClass('show') && selector.hasClass('hidden'))){
             selector.addClass('hidden');
             selector.removeClass('show');
+        }else if(selector.hasClass('show hidden')){
+            selector.removeClass('hidden');
+            selector.addClass('show');
         }else{
             selector.removeClass('hidden');
             selector.addClass('show');
         }
+        $('.subMenuItems div').each(function () {
+            if (!($(this).attr('class') === selector.attr('class'))){
+                $(this).addClass('hidden');
+            }
+        })
+    })
+
+    $('.close-mobile-sub-menu-button').click(function (event) {
+        event.preventDefault();
+        $(this).parent().removeClass('show');
+        $(this).parent().addClass('hidden');
     })
 })
