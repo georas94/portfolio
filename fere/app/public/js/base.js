e5da9d973ed5cc23be0845d07d11616ceb9552e4
georas94 <rashid.tamboura@icloud.com>
2023-12-20 15:14:58 +0100

    Fere : Update navbar behaviour for mobile screens

--- fere/app/public/js/base.js
+++ fere/app/public/js/base.js
@@ -1,32 +1,33 @@
 $(document).ready(function () {
-    $('.menu li').click(function (event) {
+    $('.menu li').on('click',function (event) {
         event.preventDefault();
         let selector = $('.sub-'+$(this).attr('class'));
+        let subMenuItemsDiv = $('.subMenuItems div');
         if (selector.hasClass('show') && !(selector.hasClass('show') && selector.hasClass('hidden'))){
             selector.addClass('hidden');
             selector.removeClass('show');
         }else if(selector.hasClass('show hidden')){
             selector.removeClass('hidden');
             selector.addClass('show');
-            if($('.subMenuItems div').is(':visible')) {
+            if(subMenuItemsDiv.is(':visible')) {
                 selector.css('position', 'fixed');
             }
 
         }else{
             selector.removeClass('hidden');
             selector.addClass('show');
-            if($('.subMenuItems div').is(':visible')) {
+            if(subMenuItemsDiv.is(':visible')) {
                 selector.css('position', 'fixed');
             }
         }
-        $('.subMenuItems div').each(function () {
+        subMenuItemsDiv.each(function () {
             if (!($(this).attr('class') === selector.attr('class'))){
                 $(this).addClass('hidden');
             }
         })
     })
 
-    $('.close-mobile-sub-menu-button').click(function (event) {
+    $('.close-mobile-sub-menu-button').on('click',function (event) {
         event.preventDefault();
         $(this).parent().removeClass('show');
         $(this).parent().addClass('hidden');
