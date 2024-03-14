c7c1652f7548c788837d3c9eb539de50560cd8ff
georas94 <rashid.tamboura@icloud.com>
2024-03-14 23:09:24 +0100

    Fere :  Fix user registration form

--- fere/app/src/Form/RegistrationFormType.php
+++ fere/app/src/Form/RegistrationFormType.php
@@ -2,15 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\ApplicationRole;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -86,6 +86,11 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ])
                 ]
+            ])
+            ->add('applicationRole', EntityType::class, [
+                'class' => ApplicationRole::class,
+                'choice_label' => 'name',
+                'empty_data' => 'friend'
             ])
         ;
     }
