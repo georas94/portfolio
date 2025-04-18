d3a5a1582d9c54ada0a38eb04a6390f81d39ae7b
georas94 <rashid.tamboura@icloud.com>
2025-04-18 05:08:14 +0200

    Update Bid repo

--- bid/app/public/index.php
+++ bid/app/public/index.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $_SERVER['TRUSTED_PROXIES'] = [$_SERVER['REMOTE_ADDR']];
+    Request::setTrustedProxies([$_SERVER['REMOTE_ADDR']], Request::HEADER_FORWARDED);
+}
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
